8e3e74258c5dfe6173d6779cfeab67ac008c9277
arifonal <132202240+arifonal@users.noreply.github.com>
2024-05-10 23:04:18 +0300

    Delete Project-Sprint2 directory

--- Project-Sprint2/checkbox.js
+++
@@ -1,5 +0,0 @@
-/*I checked it with DOM management. I used it to manage the active/deactivate states of the selected checkbox button. */
-
-$(document).ready(function () {
-    $("input[type=checkbox]").checkboxradio();
-   });
